5e2c3f4257b4fcc0ce942ac8a9acf5520a6f804c
CODE CRAFT web sistem <76017481+CODE-CRAFT-WEB-SISTEM@users.noreply.github.com>
2024-07-09 00:51:15 -0300

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,57 @@
-const CACHE_NAME = 'atividades-cache-v1';
+const CACHE_NAME = 'offline-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
   '/index.js',
-  '/manifest.json',
-  // Inclua aqui outras URLs de recursos que você deseja armazenar em cache
+  // Adicione outros arquivos que você deseja armazenar em cache
 ];
 
+// Instala o service worker e armazena em cache todos os recursos necessários
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
+// Intercepta as solicitações de rede e retorna os recursos do cache se disponíveis
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
+        // Cache hit - retorna a resposta do cache
         if (response) {
           return response;
         }
-        // No cache match - fetch from network
-        return fetch(event.request)
-          .then(response => {
-            // Check if we received a valid response
+        return fetch(event.request).then(
+          response => {
+            // Verifica se recebeu uma resposta válida
             if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
-            // Clone the response (as it's a stream) to cache
-            const responseToCache = response.clone();
+
+            // Clona a resposta
+            var responseToCache = response.clone();
+
             caches.open(CACHE_NAME)
               .then(cache => {
                 cache.put(event.request, responseToCache);
               });
+
             return response;
-          });
+          }
+        );
       })
   );
 });
 
+// Ativa o service worker e remove caches antigos se necessário
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
-
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(

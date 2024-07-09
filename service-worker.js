8b6336f8ff3cad491a655dd906bd6975d8087fbd
CODE CRAFT web sistem <76017481+CODE-CRAFT-WEB-SISTEM@users.noreply.github.com>
2024-07-09 00:12:13 -0300

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,11 @@
-// service-worker.js
-
-const CACHE_NAME = 'atividades-dia-a-dia-cache-v1';
+const CACHE_NAME = 'atividades-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
   '/index.js',
-  '/manifest.json', // Se estiver usando um manifest para PWA
-  // Inclua outros recursos estáticos que sua página utiliza
+  '/manifest.json',
+  // Inclua aqui outras URLs de recursos que você deseja armazenar em cache
 ];
 
 self.addEventListener('install', event => {
@@ -23,31 +21,26 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
-
-        // Clone the request
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest)
-          .then(response => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // Clone the response
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          });
+        return fetch(event.request);
       })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });

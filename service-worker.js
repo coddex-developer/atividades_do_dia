9eeff21c6a36394672fd8d0c8a7c9c2845e45a0d
CODE CRAFT web sistem <76017481+CODE-CRAFT-WEB-SISTEM@users.noreply.github.com>
2024-07-09 01:08:47 -0300

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
+        console.log('Cache aberto');
         return cache.addAll(urlsToCache);
       })
   );
@@ -27,15 +27,18 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
-        return fetch(event.request).then(
+        // Clona a solicitação. Uma solicitação é um stream e só pode ser consumida uma vez. Como queremos consumir a solicitação tanto para o cache quanto para a rede, devemos cloná-la.
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
           response => {
-            // Verifica se recebeu uma resposta válida
+            // Verifica se recebemos uma resposta válida
             if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
 
-            // Clona a resposta
-            var responseToCache = response.clone();
+            // Clona a resposta. Uma resposta é um stream e, assim como a solicitação, só pode ser consumida uma vez. Como queremos consumir a resposta tanto para o cache quanto para o navegador, devemos cloná-la.
+            const responseToCache = response.clone();
 
             caches.open(CACHE_NAME)
               .then(cache => {
@@ -49,7 +52,7 @@ self.addEventListener('fetch', event => {
   );
 });
 
-// Ativa o service worker e remove caches antigos se necessário
+// Atualiza o service worker e remove caches antigos
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(

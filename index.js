87097e2c5b7345cc7626aab49cd7d5888aea0999
CODE CRAFT web sistem <76017481+CODE-CRAFT-WEB-SISTEM@users.noreply.github.com>
2024-07-10 13:26:19 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let CACHE_NAME = 'offline-cache-v21'; 
+let CACHE_NAME = 'offline-cache-v22'; 
 
 document.addEventListener('DOMContentLoaded', () => {
   const versionDisplay = document.getElementById('versionDisplay');

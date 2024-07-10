50ce78b77513d02c057c8e95e347a7df092bc689
CODE CRAFT web sistem <76017481+CODE-CRAFT-WEB-SISTEM@users.noreply.github.com>
2024-07-10 13:20:45 -0300

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'offline-cache-v19';  // Defina a versão do cache aqui
+const CACHE_NAME = 'offline-cache-v20';  // Defina a versão do cache aqui
 const urlsToCache = [
   '/',
   '/index.html',

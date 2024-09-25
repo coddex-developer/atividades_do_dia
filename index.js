fe615f15c5bf34dae1fd2daa1e798cea4d57ac24
Gabriel Rodrigues Lima <gabrielrodrigueslima22@gmail.com>
2024-09-25 01:52:03 -0300

    correcoes e melhorias

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const numberVersion = 33
+const numberVersion = 35
 
 const CACHE_NAME = `offline-cache-v${numberVersion}`; 
 
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
   versionDisplay.textContent = `Versão: ${CACHE_NAME}`;
 
   const monthNames = [
-    "01", "02", "03", "04", "05", "06",
-    "07", "08", "09", "10", "11", "12"
+    "/01/", "/02/", "/03/", "/04/", "/05/", "/06/",
+    "/07/", "/08/", "/09/", "/10/", "/11/", "/12/"
   ];
 
   if ('serviceWorker' in navigator) {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'offline-cache-v34';  // Defina a versão do cache aqui
+const CACHE_NAME = 'offline-cache-v35';  // Defina a versão do cache aqui
 const urlsToCache = [
   '/',
   '/index.html',
